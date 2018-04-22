debcd35caa52f076b504bdacf7338f2356f9c3e7
Igor Kharchenko <hatand@bk.ru>
2018-04-22 17:10:31 +0700

    Add temporary redirection to post/index

--- frontend/views/site/error.php
+++ frontend/views/site/error.php
@@ -1,8 +1,16 @@
 <?php
 
-/** @var $this \yii\web\View */
 use yii\helpers\Html;
+use yii\helpers\Url;
+
+/** @var $this \yii\web\View */
+/** @var $message string */
 ?>
 
-<h1><?= Html::encode('Achtung!') ?></h1>
-<h2>Всё сломалось. Приходите попозже.</h2>
+<h1></h1>
+<h2><?= Html::encode($message) ?></h2>
+<p>
+    Произошла ошибка.
+
+    Вы можете перейти <?= Html::a('на главную страницу', Url::to(['post/index'], true)) ?>.
+</p>
